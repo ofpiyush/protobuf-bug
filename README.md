0bc9e89b79d229e698a139bc4326f2c440db18c8
Piyush Mishra <piyush@codeitout.com>
2019-07-09 15:08:50 +0530

    Bug report with source and vendor

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # protobuf-bug
+
+Steps to reproduce
+
+```bash
+# we assume that you have protoc installed already
+protoc --version
+# libprotoc 3.7.1
+protoc  --go_out=./ simple/*.proto
+dep ensure
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/ofpiyush/protobuf-bug/simple"
+)
+
+func main() {
+	source := &simple.BytesTest{
+		Data: []byte{},
+	}
+	unmarshalled := simple.BytesTest{}
+
+	pr, err := proto.Marshal(source)
+	if err != nil {
+		fmt.Println("Error in marshalling:", err)
+		return
+	}
+
+	proto.Unmarshal(pr, &unmarshalled)
+	fmt.Printf("Data in source: %#v\n", source.Data)
+	fmt.Println("Nil check result in original object:", nil == source.Data)
+	fmt.Printf("Data after Unmarshal: %#v\n", unmarshalled.Data)
+	fmt.Println("Nil check result in unmarshalled object:", nil == unmarshalled.Data)
+}
